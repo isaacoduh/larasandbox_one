279d05a7e87b2dccfed89267696782791395ab83
isaacoduh <ufuomaoduh@gmail.com>
2020-08-27 02:20:23 +0100

    Readme Update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-## CMS with Laravel and Vue
+## Quick Links
 
-A content management application for a website owned by a ficticious product company. The idea is to build out a platform for the client to be able to manage certain parts of their static website.
+Anybody can use this to access quick links all around the internet. Use case most likely is when asked to provide relevant links to a work application form
 
 ## Key Learnings and Tools
-- PHP, Laravel, ES6 (Javascript), Vue
+- PHP, Laravel, ES6 (Javascript), Axios
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,22 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/github/{username}', function($username){
+    // Setup Guzzle client to interact with api
+    // https://api.github.com/users/isaacoduh/events/public
+    // https://www.itsolutionstuff.com/post/laravel-6-guzzle-http-client-exampleexample.html
+
+    $client = new \GuzzleHttp\Client();
+    $request = $client->request('GET',"https://api.github.com/users/".$username."/events/public", ['verify' => false]);
+    $response = $request->getBody();
+
+    $data = json_decode($response->getContents());
+    $collection = collect($data);
+    $grouped = $collection->mapToGroups(function($item, $key){
+        return [$item['type'] => $item['id']];
+    });
+    dd($grouped);
+    // $collection->count();
+    // dd($collection->toArray());
+});
