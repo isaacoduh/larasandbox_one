e21f805215ef75e0db52704d311bcf54d71a16aa
isaacoduh <ufuomaoduh@gmail.com>
2020-08-29 20:27:03 +0100

    All Journals Vue

---
+++ resources/js/components/AllJournals.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h3 class="text-center">All Journals</h3><br/>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Author</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="journal in Journals" :key="journal.id">
+                    <td>{{journal.id}}</td>
+                    <td>{{journal.title}}</td>
+                    <td>{{journal.description}}</td>
+                    <td>{{journal.author}}</td>
+                    <td>{{journal.created_at}}</td>
+                    <td>{{journal.updated_at}}</td>
+                    <td>
+                        <div class="btn-group" role="group">
+                            <router-link :to="{name: 'edit', params: {id: journal.id}}" class="btn btn-primary">Edit</router-link>
+                            <button class="btn btn-danger" @click="deleteJournal(journal.id)">Delete</button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {journals: []}
+        },
+        created(){
+            this.axios.get('http://localhost:8000/api/journals').then(response => {
+                this.journals = response.journals;
+            });
+        },
+        methods: {
+            deleteJournal(id){
+                this.axios.delete(`http://localhost:8000/api/journal/delete/${id}`).then(response => {
+                    let i = this.journals.map(item => item.id).indexOf(id);
+                    this.journals.splice(i,1)
+                });
+            }
+        }
+    }
+</script>
