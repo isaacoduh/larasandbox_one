30b50eb68300f71c0a06e758f879795c1b54201b
isaacoduh <ufuomaoduh@gmail.com>
2020-09-01 01:32:25 +0100

    Added StorePost request, Updated Home controller, modified RedirectIfAuthenticated middleware to redirect to root route, Defined PostController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -21,8 +22,28 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function home()
     {
         return view('home');
     }
+
+    public function contact()
+    {
+        return view('contact');
+    }
+
+    public function blogPost($id, $welcome =1)
+    {
+        $pages = [
+            1 => ['title' => 'from page 1',],
+            2 => ['title' => 'from page 2',],
+        ];
+        $welcomes = [1 => '<b>Hello</b>', 2 => 'Welcome to '];
+
+        return view('blog-post', [
+            'data' => $pages[$id],
+            'welcome' => $welcomes[$welcome],
+        ]);
+    }
+
 }
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BlogPost;
+use App\Http\Requests\StorePost;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')->only(['create', 'store', 'edit', 'update', 'destroy']);
+    }
+
+    public function index()
+    {
+        return view('posts.index', ['posts' => BlogPost::withCount('comments')->get()]);
+    }
+
+    public function show($id)
+    {
+        return view('posts.show', ['post' => BlogPost::with('comments')->findOrFail($id)]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(StorePost $request)
+    {
+        $validatedData = $request->validated();
+        $blogPost = BlogPost::create($validatedData);
+        $request->session()->flash('status', 'Blog post was created!');
+        return redirect()->route('posts.show', ['post' => $blogPost->id]);
+    }
+
+    public function edit($id)
+    {
+        $post = BlogPost::findOrFail($id);
+        return view('posts.edit', ['post' => $post]);
+    }
+
+    public function update(StorePost $request, $id)
+    {
+        $post = BlogPost::findOrFail($id);
+        $validatedData = $request->validated();
+        $post->fill($validatedData);
+        $post->save();
+        $request->session()->flash('status', 'BlogPost was updated!');
+        return redirect()->route('posts.show', ['post' => $post->id]);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $post = BlogPost::findOrFail($id);
+        $post->delete();
+
+        $request->session()->flash('status', 'Blog post was deleted!');
+        return redirect()->route('posts.index');
+    }
+}
