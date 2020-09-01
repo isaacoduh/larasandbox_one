ac9fc609709ac4680b128c2d99ae910464cbdb93
isaacoduh <ufuomaoduh@gmail.com>
2020-09-01 01:03:30 +0100

    Seeders for Database, Blogposts, Comments, Users

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $posts = App\BlogPost::all();
+        if($posts->count() === 0){
+            $this->command->info('There are no blog posts, so there would be no comments added');
+            return;
+        }
+
+        $commentsCount = (int)$this->command->ask('How many comments do you require?',100);
+        factory(App\Comment::class, $commentsCount)->make()->each(function($comment) use ($posts){
+            $comment->blog_post_id = $posts->random()->id;
+            $comment->save();
+        });
+    }
+}
