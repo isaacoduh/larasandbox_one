f0095c0305cbec9bd857e64c8f12ba63942309c4
isaacoduh <ufuomaoduh@gmail.com>
2020-09-01 01:45:52 +0100

    Defined base user function on test class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,9 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function user()
+    {
+        return factory(User::class)->create();
+    }
 }
