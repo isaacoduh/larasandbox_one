a15128ea389773a24234b440d9ea1e22beb10fad
isaacoduh <ufuomaoduh@gmail.com>
2020-09-02 23:48:10 +0100

    Fallback Route, Vue Router Setup, History Mode, Vue Component Registration, router link, bootstrap, vbind shorthand, add-a-navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 require('./bootstrap');
 
 import VueRouter from 'vue-router';
+import Index from './Index';
 import router from './routes';
 
 window.Vue = require('vue');
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('example-2', require('./components/Example2.vue').default);
+
 Vue.use(VueRouter);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -17,4 +17,7 @@ Vue.use(VueRouter);
 const app = new Vue({
     el: '#app',
     router,
+    components: {
+        index: Index
+    }
 });
