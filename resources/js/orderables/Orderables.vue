e27573d994167dc3ae1ebc784718edf1da5540fe
isaacoduh <ufuomaoduh@gmail.com>
2020-09-03 02:14:07 +0100

    Promises in Javascript

--- resources/js/orderables/Orderables.vue
+++ resources/js/orderables/Orderables.vue
@@ -46,6 +46,15 @@ export default {
     },
     created() {
         this.loading = true;
+        const p = new Promise((resolve, reject) => {
+            console.log(resolve);
+            console.log(reject);
+            setTimeout(() => resolve('Hello'),3000);
+        }).then(result => "Hello Again " + result)
+        .then(result => console.log(result))
+        .catch(result => console.log(`Error ${result}`));
+        console.log(p);
+
         setTimeout(() => {
             this.bookables = [
                 {
