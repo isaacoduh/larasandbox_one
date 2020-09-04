c51d1d6ae4a4fa9ae86a38c269c572335005b9a1
isaacoduh <ufuomaoduh@gmail.com>
2020-09-04 01:23:51 +0100

    Star Rating Component, Star Rating Star Logic, Review List Cleanup

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,12 +4,14 @@ import moment from "moment";
 import VueRouter from 'vue-router';
 import Index from './Index';
 import router from './routes';
+import StarRating from './shared/components/StarRating';
 
 window.Vue = require('vue');
 
 
 Vue.use(VueRouter);
 Vue.filter("fromNow", value => moment(value).fromNow());
+Vue.component("star-rating", StarRating);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
--- resources/js/orderable/ReviewList.vue
+++ resources/js/orderable/ReviewList.vue
@@ -6,7 +6,7 @@
             <div class="border-bottom d-none d-md-block" v-for="(review, index) in reviews" :key="index">
                 <div class="row pt-4">
                     <div class="col-md-6">Isaac Oduh</div>
-                    <div class="col-md-6 d-flex justify-content-end">{{review.rating}}</div>
+                    <div class="col-md-6 d-flex justify-content-end"> <star-rating :rating="review.rating" class="fa-lg"></star-rating></div>
                 </div>
                 <div class="row">
                     <div class="col-md-12">{{review.created_at | fromNow}}</div>
