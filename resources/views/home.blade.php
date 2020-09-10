09403d9545e1ec05ccf15681c81ba6b927c47eb2
isaacoduh <ufuomaoduh@gmail.com>
2020-09-10 02:20:45 +0100

    Setup Views for Admin and Regular user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Hi there user!, Welcome !') }}
                 </div>
             </div>
         </div>
