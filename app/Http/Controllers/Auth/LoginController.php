5c4c7d2ee0145dd0107b69aaf483ec3d962796cd
isaacoduh <ufuomaoduh@gmail.com>
2020-09-10 01:59:42 +0100

    Modify Login Controller to add admin form and login methods

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -30,11 +32,39 @@ class LoginController extends Controller
 
     /**
      * Create a new controller instance.
+     * Middleware to restrict access to this controller or its methods. It is important we defined all the types of guests in the controller.
+     * This way, if one type of user is logged in, and you try to use another user to login, it will redirect to a predefined authentication page.
      *
      * @return void
      */
     public function __construct()
     {
+
         $this->middleware('guest')->except('logout');
+        $this->middleware('guest:admin')->except('logout');
+    }
+
+    /**
+     * Admin Form
+     */
+
+    public function showAdminLogin()
+    {
+        return view('auth.login', ['url' => 'admin']);
+    }
+    /**
+     * Admin Login
+     */
+    public function adminLogin(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+        if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))){
+            return redirect()->intended('/admin');
+        }
+
+        return back()->withInput($request->only('email', 'remember'));
     }
 }
