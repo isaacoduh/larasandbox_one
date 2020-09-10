eb9a387fd2b2fdcc384096ada3ccfbf7ddbe15b7
isaacoduh <ufuomaoduh@gmail.com>
2020-09-10 23:38:58 +0100

    Login Views modified, app views loaded

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,52 +19,64 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <style>
+        /*
+            these styles will animate bootstrap alerts.
+        */
+        .alert{
+            z-index: 99;
+            top: 60px;
+            right:18px;
+            min-width:30%;
+            position: fixed;
+            animation: slide 0.5s forwards;
+        }
+        @keyframes slide {
+            100% { top: 30px; }
+        }
+        @media screen and (max-width: 668px) {
+            .alert{ /* center the alert on small screens */
+                left: 10px;
+                right: 10px;
+            }
+        }
+    </style>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+    @include('inc.navbar')
+    <main class="container mt-4">
+        @yield('content')
+    </main>
+    <script src="{{asset('js/app.js')}}"></script>
 
-                    </ul>
+    {{-- Success Alert --}}
+    @if (session('status'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{session('status')}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                       <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Hi There <span class="caret"></span>
-                            </a>
+    {{-- Error Alert --}}
+    @if (session('error'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{session('error')}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    <script>
+        // close the alert after 3 seconds
+        $(document).ready(function(){
+            setTimeout(function(){
+                $(".alert").alert('close');
+            },3000);
+        });
+    </script>
 </body>
 </html>
