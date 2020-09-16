3edf8e84b1f906eb489ccca6c8ebc2dcb9f42f82
isaacoduh <ufuomaoduh@gmail.com>
2020-09-16 12:58:40 +0100

    Routes for API and Web

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:api']], function () {
+    // Permissions
+    Route::apiResource('permissions', 'PermissionsApiController');
+
+    // Roles
+    Route::apiResource('roles', 'RolesApiController');
+
+    // Users
+    Route::apiResource('users', 'UsersApiController');
+
+    // Countries
+    Route::apiResource('countries', 'CountriesApiController');
+
+    // Cities
+    Route::apiResource('cities', 'CitiesApiController');
+
+    // Trips
+    Route::apiResource('trips', 'TripsApiController');
 });
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::redirect('/', 'login');
+Route::get('/home', function(){
+    if(session('status')){
+        return redirect()->route('admin.home')->with('status', session('status'));
+    }
+    return redirect()->route('admin.home');
 });
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group([
+    'prefix' => 'user',
+    'as' => 'user.',
+    'namespace' => 'User',
+    'middleware' => ['auth']
+], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+});
+
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.',
+    'namespace' => 'Admin',
+    'middleware' => ['auth', 'admin']], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
+    Route::resource('permissions', 'PermissionsController');
+    // Roles
+    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
+    Route::resource('roles', 'RolesController');
+
+    // Users
+    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
+    Route::resource('users', 'UsersController');
+
+    // Countries
+    Route::delete('countries/destroy', 'CountriesController@massDestroy')->name('countries.massDestroy');
+    Route::resource('countries', 'CountriesController');
+
+    // Cities
+    Route::delete('cities/destroy', 'CitiesController@massDestroy')->name('cities.massDestroy');
+    Route::resource('cities', 'CitiesController');
+
+    // Bookings
+    Route::delete('bookings/destroy', 'BookingsController@massDestroy')->name('bookings.massDestroy');
+    Route::resource('bookings', 'BookingsControllers');
+});
+
+Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 'middleware' => ['auth']], function(){
+    if(file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php'))){
+        Route::get('password', 'ChangePasswordController@edit')->name('password.edit');
+        Route::get('password', 'ChangePasswordController@update')->name('password.update');
+    }
+});
